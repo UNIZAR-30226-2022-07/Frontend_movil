db2c1a4e38fd90fd7c2c1109b965e1f3aee22e1c
paulaEzpe <800692@unizar.es>
2022-04-12 22:00:51 +0200

    register working7

--- lib/src/pages/partida.dart
+++ lib/src/pages/partida.dart
@@ -14,20 +14,47 @@ class Partida extends StatefulWidget {
 class _PartidaState extends State<Partida> {
   
   @override
-  Widget buildCard(int index) => Container(
-    margin: const EdgeInsets.symmetric(
+  Widget buildCard(String carta) => GestureDetector(
+    onTap: () {
+      print("Container clicked");
+    },
+    child: Container(
+      margin: const EdgeInsets.symmetric(
       horizontal: 1, vertical: 10
-    ),
-    width: 70,
-    height: 130,
-    decoration: BoxDecoration(
-      image: DecorationImage(
-            image: AssetImage('images/uno.jpg'),
-            fit: BoxFit.fill
       ),
-      borderRadius: BorderRadius.circular(15),
-    ),
-    child: Center(child: Text('$index')),
+      width: 70,
+      height: 120,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+              image: AssetImage(carta),
+              fit: BoxFit.fill
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      // child: Center(child: Text('$index')),
+    )
+  );
+
+  @override
+  Widget cartaRobar(int index) => GestureDetector(
+    onTap: () {
+      print("Container clicked");
+    },
+    child: Container(
+      margin: const EdgeInsets.symmetric(
+      horizontal: 1, vertical: 10
+      ),
+      width: 70,
+      height: 120,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+              image: AssetImage('images/uno.jpg'),
+              fit: BoxFit.fill
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      // child: Center(child: Text('$index')),
+    )
   );
   
   @override
@@ -35,58 +62,81 @@ class _PartidaState extends State<Partida> {
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
      return Container(
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 10, 151, 24),
+          color: Color.fromARGB(255, 6, 104, 16),
         ),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.all(12),
-          child: Column (
-            children: <Widget>[
-              SizedBox(
-                height:80,
-                width: 20,
-              ), 
-              Row(
-                children: [
-                  Column(
-
+        child: Column(
+          children: [
+            SizedBox(
+                    height:80,
+                    width: 20,
+            ), 
+            Row(
+              children: [
+                SizedBox(
+                  width: 350,
+                ),
+                
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    buildCard('images/azul.jpg'),
+                  ]
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    cartaRobar(20),
+                  ]
+                )
+              ],
+              
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(12),
+              child: Column (
+                children: <Widget>[
+                  SizedBox(
+                    height:25,
+                  ), 
+                  Row(
+                    children: [
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                      buildCard('images/azul.jpg'),
+                      const SizedBox(width: 12),
+                    ]
                   )
                 ],
               ),
-              SizedBox(
-                height:140,
-                width: 20,
-              ), 
-              Row(
-                children: [
-                  buildCard(1),
-                  const SizedBox(width: 12),
-                  buildCard(2),
-                  const SizedBox(width: 12),
-                  buildCard(3),
-                  const SizedBox(width: 12),
-                  buildCard(4),
-                  const SizedBox(width: 12),
-                  buildCard(5),
-                  const SizedBox(width: 12),
-                  buildCard(6),
-                  const SizedBox(width: 12),
-                  buildCard(7),
-                  const SizedBox(width: 12),
-                  buildCard(8),
-                  const SizedBox(width: 12),
-                  buildCard(9),
-                  const SizedBox(width: 12),
-                  buildCard(10),
-                  const SizedBox(width: 12),
-                  buildCard(11),
-                  const SizedBox(width: 12),
-                  buildCard(12),
-                  const SizedBox(width: 12),
-                ]
-              )
-            ],
-          ),
+            )
+
+          ],
         )
       );
   }
